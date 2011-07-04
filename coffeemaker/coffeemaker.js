18b78cc48b3fd6d5bb8dda4120ada125ba7220dd
Thomas Frank <thomas.frank@studentlittteratur.se>
2011-07-04 16:27:03 +0200

    Fixed som indents...
    Signed-off-by: Thomas Frank <thomas.frank@studentlittteratur.se>

--- coffeemaker/coffeemaker.js
+++ coffeemaker/coffeemaker.js
@@ -1,261 +1,261 @@
 /*
-	CoffeeMaker - compiles CoffeeScript clientside
-	and caches it on the serverside
-	with no need for node.js installation
-	
-	Includes the CoffeeScript Compiler
-	MIT-licensed, Copyright Jeremy Ashkenas
-	
-	Uses Marijn Haverbeke's online UglifyJS server
-	UglifyJS is written by Mihai Bazon
-	(the parser is a port of Marijns H. parse-js for Lisp)
-	 
-	Copyright 2011 Thomas Frank
-	MIT-licensed...
+  CoffeeMaker - compiles CoffeeScript clientside
+  and caches it on the serverside
+  with no need for node.js installation
+  
+  Includes the CoffeeScript Compiler
+  MIT-licensed, Copyright Jeremy Ashkenas
+  
+  Uses Marijn Haverbeke's online UglifyJS server
+  UglifyJS is written by Mihai Bazon
+  (the parser is a port of Marijns H. parse-js for Lisp)
+   
+  Copyright 2011 Thomas Frank
+  MIT-licensed...
 */
 
 (function(){
-	
-	// Synchronous ajax post
-	var getFile = function(url, passData) {
-		if (window.XMLHttpRequest) {              
-			AJAX=new XMLHttpRequest();              
-		} else {                                  
-			AJAX=new ActiveXObject("Microsoft.XMLHTTP");
-		};
-		if (AJAX) {
-			AJAX.open("POST", url, false);
-			AJAX.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			AJAX.send(passData);
-			if(AJAX.responseText.indexOf('<b>Warning</b>:') >=0 
-				&& AJAX.responseText.indexOf('<br />') == 0){
-					alert("CoffeeMaker: Check permissions for cache folder!")
-					location.href="javascript:void(0)"
-			}
-			return AJAX.responseText;                                         
-		} else {
-			 return false;
-		}                                             
-	};
-	
-	// Strip inner strings
-	var stringStripper = {
-		remove : function(str){
-			var inStr = false, strMem = [], ar = [], i, co = 0, x;
-			x = str.split('');
-			for(i = 0;i < x.length; i++){
-				// start of string
-				if(!inStr && (x[i] == '"' || x[i] == "'")){
-					 inStr = x[i];
-					 strMem.push([inStr]);
-					 ar.push('_-_strMem' + co + '_-_');
-					 co++
-					 continue
-				};
-				// end of string
-				if(inStr && x[i-1]!='\\' && x[i] == inStr){
-					strMem[strMem.length-1].push(inStr);
-					inStr = false;
-					continue 
-				};
-				// push characters
-				(inStr ? strMem[strMem.length-1] : ar).push(x[i]);
-			};
-			// join arrays to strings
-			ar = ar.join('');
-			for(i = 0; i < strMem.length; i++){
-				strMem[i] = strMem[i].join('')
-			}
-			// return stripped string and string memory
-			return {
-				stripped: ar,
-				strMem: strMem
-			}
-		},
-		restore: function(stripped,strMem){
-			// reinsert strings
-			var x = stripped, i;
-			for(i = 0; i < strMem.length;i++){
-				x = x.split('_-_strMem' + i + '_-_').join(strMem[i])
-			};
-			return x
-		}
-	};
-	
-	// Do per line 
-	var lineDo = function(str,eachLineFunc){
-		var i, stripperObj = stringStripper.remove(str);
-		var x = stripperObj.stripped;
-		var l = x.split('\n')
-		for(i = l.length - 1; i >= 0; i--){
-			l[i] = eachLineFunc(l[i],l,i);	
-		};
-		for(i = l.length - 1; i >= 0; i--){
-			if(l[i] == '_-_markedfordeletion_-_'){
-				l.splice(i,1);
-			}
-		};
-		x = l.join('\n');
-		x = stringStripper.restore(x,stripperObj.strMem);
-		return x
-	};
-	
-	// Do includes
-	var allIncludes;
-	var doIncludes = function(x,path,subinclude){
-		
-		!subinclude && (allIncludes = []);
-		
-		path = path.substring(0,path.lastIndexOf('/')+1);
-		
-		x = lineDo(x,function(l){
-			if(l && !l.replace(/\s*#\s*include.*/,'')){
-				var wspace = l.replace(/(\s*).*/,'$1');
-				var toinclude = l.replace(/\s*#\s*include(.*)/,'$1');
-				toinclude = toinclude.replace(/\s*/g,'');
-				l = wspace + '_-_include_-_' + toinclude
-			};
-			return l
-		});
-		
-		x = x.split('\n');
-		var l, ifile, content, wspace, i, j, js, pathy;
-		for(i = 0; i < x.length; i++){
-			l = x[i];
-			if (l.indexOf('_-_include_-_') >= 0){
-				wspace = l.replace(/(\s*).*/,'$1');
-				ifile = path + l.split('_-_include_-_').join('').
-					split('"').join('').split("'").join('').replace(/\s*/g,'');
-				content = getFile(ifile);
-				pathy = ifile.substring(ifile.indexOf('=')+1);
-				content && allIncludes.push(pathy);
-				content = doIncludes(content,ifile,true);
-				if(content.split(/\w/).length < 2){
-					l = '';
-				}
-				else {
-					content = content.split('\n');
-					l = wspace + '#include ' + pathy;
-					js = ifile.substring(ifile.length-3) == '.js';
-					for(j = 0; j < content.length; j++){
-						l += '\n' + wspace + (js && j==0 ? '`' : '') 
-						+ content[j] + (js && j+1 == content.length ? '`' : '') ;
-					};
-					l += '\n';
-				}
-			}
-			x[i] = l;
-		};
-		x = x.join('\n');
-		return x
-	};
-	
-	
-	// Preprocess the coffee script file
-	var preProcessCoffee = function(x){
-		
-		x = !coffeemaker.keepBlankLines ? x : 	lineDo(x,function(l,whole,co){
-			if(!l.replace(/\s*/,'')){
-				var wspace = whole[whole[co+1] ? co+1 : co-1].replace(/(\s*).*/,'$1');
-				l = wspace + '# _-_isABlankLine_-_'
-			}
-			return l
-		});
-		
-		x = !(coffeemaker.keepCoffeeComments || coffeemaker.keepBlankLines) ? x :
-		lineDo(x,function(l){
-			if(!l.replace(/\s*#.*/,'')){
-				if(!coffeemaker.keepCoffeeComments && l.indexOf('_-_isABlankLine_-_') < 0){return};
-				l = l.replace(/(\s*)#\s*(.*)/g,
-				'$1###\n_-_onelinecoffeecomment_-_\n$2\n###\n');
-			}
-			return l
-		});
-		
-	  return x
-	};
-	
-	// Postprocess the compiled javascript
-	var postProcessJS = function(x){
-		
-		x = !coffeemaker.splitVarDeclarations ? x :
-		lineDo(x,function(l,whole,co){
-				if(!l.replace(/\s*var\s{1,}.*/,'')){
-					var wspace = l.substring(0,l.indexOf('var')) + '    ';
-					l = l.split(/\,\s*/);
-					for(var i = l.length - 1; i >= 0; i--){
-						if(l[i].indexOf('__bind')>=0){
-							l[i] += ', ' + l[i+1] + ', ' +l[i+2];
-							l.splice(i+1,2);
-						};
-						if(l[i].indexOf('__extends')>=0){
-							l[i] += ', ' + l[i+1];
-							l.splice(i+1,1);
-							for(var j = co + 1; j < co + 8; j++){
-								whole[j] = wspace + whole[j]
-							}
-							whole[co+7] += '\n';
-						};
-					};
-					l = l.join(',\n' + wspace);
-					l += l.indexOf('__extends') < 0 ? '\n' : '';
-				};
-				return l
-		});
-		
-		x = !(coffeemaker.keepCoffeeComments || coffeemaker.keepBlankLines) ? x :
-		lineDo(x,function(l,whole,co){
-			if(l.indexOf('_-_onelinecoffeecomment_-_') >= 0){
-				var c = whole[co+1];
-				var co2 = whole[co+2].replace(/\*\/\s*/,'');
-				whole[co+2] = !co2.replace(/\s*/g,'') ? '_-_markedfordeletion_-_' : co2;
-				whole[co+1] = whole[co-1] = '_-_markedfordeletion_-_';
-				l = c.replace(/(\s*)(.*)/,'$1// $2');
-				if(l.indexOf('_-_isABlankLine_-_') >=0){l = ''}
-			};
-			return l
-		});
-		
-		x = !coffeemaker.splitConcatenations ? x :
-		lineDo(x,function(l){
-			wspace = l.replace(/(\s*).*/,'$1');
-			l = l.split('++').join('_-_doublePlussesHere_-_').split('+');
-			var k = l[0];
-			l[0] = l[0].split(wspace).join('');
-			for(var i = 1; i < l.length; i++){
-				k +=  k.split('\n').pop().length >= 40 ?  '\n' 
-					+ wspace + '+' + l[i] : '+' + l[i];
-			}
-			return k.split('_-_doublePlussesHere_-_').join('++')
-		});
-		
-		return x
-	};
-	
-	// Compile
-	var brewmem=',';
-	coffeemaker.compile = function(coffeeFile){
-		if(brewmem.indexOf(',' + coffeeFile + ',') >= 0){return};
-		//brewmem += coffeeFile + ',';
-		this.tocompile = undefined;
-		var x = getFile(coffeeFile);
-		x = coffeemaker.allowIncludes ? doIncludes(x,coffeeFile) : x;
-		x = coffeemaker.uglifyJS ? x : preProcessCoffee(x);
-		var y = CoffeeScript.compile(x);
-		y = coffeemaker.uglifyJS ? y : postProcessJS(y);
-		getFile(coffeeFile.split('?')[0],'path=' 
-			+ encodeURIComponent(coffeeFile.split('passthru=')[1]) 
-			+ '&store=' + encodeURIComponent(y)
-			+ (!allIncludes ? '' : '&includes=' 
-			+ encodeURIComponent(allIncludes.join('\n  ')))
-			+ '&ticket=' + encodeURIComponent(this.ticket)
-		);
-		return true
-	};
-	
-	// First compile...
-	if(coffeemaker.tocompile){
-		coffeemaker.compile(coffeemaker.tocompile);
-	};
+  
+  // Synchronous ajax post
+  var getFile = function(url, passData) {
+    if (window.XMLHttpRequest) {              
+      AJAX=new XMLHttpRequest();              
+    } else {                                  
+      AJAX=new ActiveXObject("Microsoft.XMLHTTP");
+    };
+    if (AJAX) {
+      AJAX.open("POST", url, false);
+      AJAX.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      AJAX.send(passData);
+      if(AJAX.responseText.indexOf('<b>Warning</b>:') >=0 
+        && AJAX.responseText.indexOf('<br />') == 0){
+          alert("CoffeeMaker: Check permissions for cache folder!")
+          location.href="javascript:void(0)"
+      }
+      return AJAX.responseText;                                         
+    } else {
+       return false;
+    }                                             
+  };
+  
+  // Strip inner strings
+  var stringStripper = {
+    remove : function(str){
+      var inStr = false, strMem = [], ar = [], i, co = 0, x;
+      x = str.split('');
+      for(i = 0;i < x.length; i++){
+        // start of string
+        if(!inStr && (x[i] == '"' || x[i] == "'")){
+           inStr = x[i];
+           strMem.push([inStr]);
+           ar.push('_-_strMem' + co + '_-_');
+           co++
+           continue
+        };
+        // end of string
+        if(inStr && x[i-1]!='\\' && x[i] == inStr){
+          strMem[strMem.length-1].push(inStr);
+          inStr = false;
+          continue 
+        };
+        // push characters
+        (inStr ? strMem[strMem.length-1] : ar).push(x[i]);
+      };
+      // join arrays to strings
+      ar = ar.join('');
+      for(i = 0; i < strMem.length; i++){
+        strMem[i] = strMem[i].join('')
+      }
+      // return stripped string and string memory
+      return {
+        stripped: ar,
+        strMem: strMem
+      }
+    },
+    restore: function(stripped,strMem){
+      // reinsert strings
+      var x = stripped, i;
+      for(i = 0; i < strMem.length;i++){
+        x = x.split('_-_strMem' + i + '_-_').join(strMem[i])
+      };
+      return x
+    }
+  };
+  
+  // Do per line 
+  var lineDo = function(str,eachLineFunc){
+    var i, stripperObj = stringStripper.remove(str);
+    var x = stripperObj.stripped;
+    var l = x.split('\n')
+    for(i = l.length - 1; i >= 0; i--){
+      l[i] = eachLineFunc(l[i],l,i);  
+    };
+    for(i = l.length - 1; i >= 0; i--){
+      if(l[i] == '_-_markedfordeletion_-_'){
+        l.splice(i,1);
+      }
+    };
+    x = l.join('\n');
+    x = stringStripper.restore(x,stripperObj.strMem);
+    return x
+  };
+  
+  // Do includes
+  var allIncludes;
+  var doIncludes = function(x,path,subinclude){
+    
+    !subinclude && (allIncludes = []);
+    
+    path = path.substring(0,path.lastIndexOf('/')+1);
+    
+    x = lineDo(x,function(l){
+      if(l && !l.replace(/\s*#\s*include.*/,'')){
+        var wspace = l.replace(/(\s*).*/,'$1');
+        var toinclude = l.replace(/\s*#\s*include(.*)/,'$1');
+        toinclude = toinclude.replace(/\s*/g,'');
+        l = wspace + '_-_include_-_' + toinclude
+      };
+      return l
+    });
+    
+    x = x.split('\n');
+    var l, ifile, content, wspace, i, j, js, pathy;
+    for(i = 0; i < x.length; i++){
+      l = x[i];
+      if (l.indexOf('_-_include_-_') >= 0){
+        wspace = l.replace(/(\s*).*/,'$1');
+        ifile = path + l.split('_-_include_-_').join('').
+          split('"').join('').split("'").join('').replace(/\s*/g,'');
+        content = getFile(ifile);
+        pathy = ifile.substring(ifile.indexOf('=')+1);
+        content && allIncludes.push(pathy);
+        content = doIncludes(content,ifile,true);
+        if(content.split(/\w/).length < 2){
+          l = '';
+        }
+        else {
+          content = content.split('\n');
+          l = wspace + '#include ' + pathy;
+          js = ifile.substring(ifile.length-3) == '.js';
+          for(j = 0; j < content.length; j++){
+            l += '\n' + wspace + (js && j==0 ? '`' : '') 
+            + content[j] + (js && j+1 == content.length ? '`' : '') ;
+          };
+          l += '\n';
+        }
+      }
+      x[i] = l;
+    };
+    x = x.join('\n');
+    return x
+  };
+  
+  
+  // Preprocess the coffee script file
+  var preProcessCoffee = function(x){
+    
+    x = !coffeemaker.keepBlankLines ? x :   lineDo(x,function(l,whole,co){
+      if(!l.replace(/\s*/,'')){
+        var wspace = whole[whole[co+1] ? co+1 : co-1].replace(/(\s*).*/,'$1');
+        l = wspace + '# _-_isABlankLine_-_'
+      }
+      return l
+    });
+    
+    x = !(coffeemaker.keepCoffeeComments || coffeemaker.keepBlankLines) ? x :
+    lineDo(x,function(l){
+      if(!l.replace(/\s*#.*/,'')){
+        if(!coffeemaker.keepCoffeeComments && l.indexOf('_-_isABlankLine_-_') < 0){return};
+        l = l.replace(/(\s*)#\s*(.*)/g,
+        '$1###\n_-_onelinecoffeecomment_-_\n$2\n###\n');
+      }
+      return l
+    });
+    
+    return x
+  };
+  
+  // Postprocess the compiled javascript
+  var postProcessJS = function(x){
+    
+    x = !coffeemaker.splitVarDeclarations ? x :
+    lineDo(x,function(l,whole,co){
+        if(!l.replace(/\s*var\s{1,}.*/,'')){
+          var wspace = l.substring(0,l.indexOf('var')) + '    ';
+          l = l.split(/\,\s*/);
+          for(var i = l.length - 1; i >= 0; i--){
+            if(l[i].indexOf('__bind')>=0){
+              l[i] += ', ' + l[i+1] + ', ' +l[i+2];
+              l.splice(i+1,2);
+            };
+            if(l[i].indexOf('__extends')>=0){
+              l[i] += ', ' + l[i+1];
+              l.splice(i+1,1);
+              for(var j = co + 1; j < co + 8; j++){
+                whole[j] = wspace + whole[j]
+              }
+              whole[co+7] += '\n';
+            };
+          };
+          l = l.join(',\n' + wspace);
+          l += l.indexOf('__extends') < 0 ? '\n' : '';
+        };
+        return l
+    });
+    
+    x = !(coffeemaker.keepCoffeeComments || coffeemaker.keepBlankLines) ? x :
+    lineDo(x,function(l,whole,co){
+      if(l.indexOf('_-_onelinecoffeecomment_-_') >= 0){
+        var c = whole[co+1];
+        var co2 = whole[co+2].replace(/\*\/\s*/,'');
+        whole[co+2] = !co2.replace(/\s*/g,'') ? '_-_markedfordeletion_-_' : co2;
+        whole[co+1] = whole[co-1] = '_-_markedfordeletion_-_';
+        l = c.replace(/(\s*)(.*)/,'$1// $2');
+        if(l.indexOf('_-_isABlankLine_-_') >=0){l = ''}
+      };
+      return l
+    });
+    
+    x = !coffeemaker.splitConcatenations ? x :
+    lineDo(x,function(l){
+      wspace = l.replace(/(\s*).*/,'$1');
+      l = l.split('++').join('_-_doublePlussesHere_-_').split('+');
+      var k = l[0];
+      l[0] = l[0].split(wspace).join('');
+      for(var i = 1; i < l.length; i++){
+        k +=  k.split('\n').pop().length >= 40 ?  '\n' 
+          + wspace + '+' + l[i] : '+' + l[i];
+      }
+      return k.split('_-_doublePlussesHere_-_').join('++')
+    });
+    
+    return x
+  };
+  
+  // Compile
+  var brewmem=',';
+  coffeemaker.compile = function(coffeeFile){
+    if(brewmem.indexOf(',' + coffeeFile + ',') >= 0){return};
+    //brewmem += coffeeFile + ',';
+    this.tocompile = undefined;
+    var x = getFile(coffeeFile);
+    x = coffeemaker.allowIncludes ? doIncludes(x,coffeeFile) : x;
+    x = coffeemaker.uglifyJS ? x : preProcessCoffee(x);
+    var y = CoffeeScript.compile(x);
+    y = coffeemaker.uglifyJS ? y : postProcessJS(y);
+    getFile(coffeeFile.split('?')[0],'path=' 
+      + encodeURIComponent(coffeeFile.split('passthru=')[1]) 
+      + '&store=' + encodeURIComponent(y)
+      + (!allIncludes ? '' : '&includes=' 
+      + encodeURIComponent(allIncludes.join('\n  ')))
+      + '&ticket=' + encodeURIComponent(this.ticket)
+    );
+    return true
+  };
+  
+  // First compile...
+  if(coffeemaker.tocompile){
+    coffeemaker.compile(coffeemaker.tocompile);
+  };
 
 })();
--- coffeemaker/coffeemaker.php
+++ coffeemaker/coffeemaker.php
@@ -1,24 +1,24 @@
 <?
 /*
-	CoffeeMaker - compiles CoffeeScript clientside
-	and caches it on the serverside
-	with no need for node.js installation
-	
-	Please Note: 
-	For safety you are adviced to turn off cache creation 
-	in a production environment.
-	
-	Includes the CoffeeScript Compiler
-	MIT-licensed, Copyright Jeremy Ashkenas
-	
-	Uses Marijn Haverbeke's online UglifyJS server
-	UglifyJS is written by Mihai Bazon
-	(the parser is a port of Marijns H. parse-js for Lisp)
-	
-	Icon from http://www.visualpharm.com
-	 
-	Copyright 2011 Thomas Frank
-	MIT-licensed...
+  CoffeeMaker - compiles CoffeeScript clientside
+  and caches it on the serverside
+  with no need for node.js installation
+  
+  Please Note: 
+  For safety you are adviced to turn off cache creation 
+  in a production environment.
+  
+  Includes the CoffeeScript Compiler
+  MIT-licensed, Copyright Jeremy Ashkenas
+  
+  Uses Marijn Haverbeke's online UglifyJS server
+  UglifyJS is written by Mihai Bazon
+  (the parser is a port of Marijns H. parse-js for Lisp)
+  
+  Icon from http://www.visualpharm.com
+   
+  Copyright 2011 Thomas Frank
+  MIT-licensed...
 */
 
 // version number off Coffeemaker
@@ -33,9 +33,9 @@ header("Content-type: application/x-javascript");
 
 // check if paths are safe (within document_root) else die
 function okPath($x){
-	$x = realpath($_SERVER['DOCUMENT_ROOT'].$x);
-	$y = explode($_SERVER['DOCUMENT_ROOT'],$x);
-	count($y) < 2 && die();
+  $x = realpath($_SERVER['DOCUMENT_ROOT'].$x);
+  $y = explode($_SERVER['DOCUMENT_ROOT'],$x);
+  count($y) < 2 && die();
 }
 isset($_GET['passthru']) && okPath($_GET['passthru']);
 isset($_GET['file']) && okPath($_GET['file']);
@@ -43,8 +43,8 @@ isset($_POST['path']) && okPath($_POST['path']);
 
 // return the content of a .cof file
 if(isset($_GET['passthru'])){
-	$x = $_SERVER['DOCUMENT_ROOT'].$_GET['passthru'];
-	die(file_exists($x) ? file_get_contents($x) : '');
+  $x = $_SERVER['DOCUMENT_ROOT'].$_GET['passthru'];
+  die(file_exists($x) ? file_get_contents($x) : '');
 };
 
 // files involved
@@ -65,12 +65,12 @@ $includes = explode("Includes:",$includes);
 $includes = $includes[1];
 $includes = explode("\n  ",$includes);
 foreach ($includes as $ifile){
-	if($ifile){
-		$f = $_SERVER['DOCUMENT_ROOT'].$ifile;
-		if(file_exists($f) && filemtime($f) > $COF_f_time){
-			$COF_f_time = filemtime($f);
-		}
-	}
+  if($ifile){
+    $f = $_SERVER['DOCUMENT_ROOT'].$ifile;
+    if(file_exists($f) && filemtime($f) > $COF_f_time){
+      $COF_f_time = filemtime($f);
+    }
+  }
 };
 
 // check cached javascript file for latest time of change
@@ -78,74 +78,74 @@ $JS_f_time = $JS_f_exists ? filemtime($JS_file) : 0;
 
 // save compiled version
 if($allowCacheCreation && isset($_POST['path']) && isset($_POST['store'])){
-	if(str_replace('__tickets.txt','',$_POST['path']) != $_POST['path']){die();}
-	if(!isset($_POST['ticket'])){die("No ticket");}
-	$tickets  = explode("\n",file_get_contents('cache/__tickets.txt'));
-	if(!in_array($_POST['ticket'],$tickets)){
-		die('<br /><b>Warning</b>:');
-	}
-	else {
-		file_put_contents('cache/__tickets.txt',str_replace($_POST['ticket']."\n",'',implode("\n",$tickets)));
-	}
-	$path = dirname(__FILE__).'/cache'.str_replace('.cof','.js',str_replace('..','',$_POST['path']));
-	if(!file_exists($path)){
-		mkdir(dirname($path),0777,true);
-	}
-	else {
-		unlink($path);
-	}
-	$readVer = explode('CoffeeScript',file_get_contents('coffee-script.js'));
-	$readVer = explode("\n",$readVer[1]);
-	$readVer = $readVer[0];
-	$store = utf8_encode($_POST['store']);
-	if($uglifyJS){
-		$session = curl_init('http://marijnhaverbeke.nl/uglifyjs');
-		curl_setopt ($session, CURLOPT_POST, true);
-		curl_setopt ($session, CURLOPT_POSTFIELDS, 'js_code='.urlencode($store));
-		curl_setopt($session, CURLOPT_HEADER, false);
-		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-		$store = curl_exec($session);
-		curl_close($session);
-	};
-	$toStore = "/*\n  Auto-generated JavaScript, using CoffeeMaker v".$version."\n".
-		"  and the CoffeeScript".$readVer.
-		"\n\n  CoffeeScript last modified: ".date("F d Y H:i:s", $COF_f_time ).
-		"\n  Compiled to JavaScript:     ".date ("F d Y H:i:s").
-		($uglifyJS ? "\n  Minified using UglifyJS." : '').
-		($_POST['includes'] ? "\n\n  Includes:\n  ".$_POST['includes']."\n" : '').
-		"\n*/\n\n".$store;
-	$toStore = utf8_encode(str_replace("\r","",$toStore));
-	file_put_contents($path,$toStore);
-	die();
+  if(str_replace('__tickets.txt','',$_POST['path']) != $_POST['path']){die();}
+  if(!isset($_POST['ticket'])){die("No ticket");}
+  $tickets  = explode("\n",file_get_contents('cache/__tickets.txt'));
+  if(!in_array($_POST['ticket'],$tickets)){
+    die('<br /><b>Warning</b>:');
+  }
+  else {
+    file_put_contents('cache/__tickets.txt',str_replace($_POST['ticket']."\n",'',implode("\n",$tickets)));
+  }
+  $path = dirname(__FILE__).'/cache'.str_replace('.cof','.js',str_replace('..','',$_POST['path']));
+  if(!file_exists($path)){
+    mkdir(dirname($path),0777,true);
+  }
+  else {
+    unlink($path);
+  }
+  $readVer = explode('CoffeeScript',file_get_contents('coffee-script.js'));
+  $readVer = explode("\n",$readVer[1]);
+  $readVer = $readVer[0];
+  $store = utf8_encode($_POST['store']);
+  if($uglifyJS){
+    $session = curl_init('http://marijnhaverbeke.nl/uglifyjs');
+    curl_setopt ($session, CURLOPT_POST, true);
+    curl_setopt ($session, CURLOPT_POSTFIELDS, 'js_code='.urlencode($store));
+    curl_setopt($session, CURLOPT_HEADER, false);
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+    $store = curl_exec($session);
+    curl_close($session);
+  };
+  $toStore = "/*\n  Auto-generated JavaScript, using CoffeeMaker v".$version."\n".
+    "  and the CoffeeScript".$readVer.
+    "\n\n  CoffeeScript last modified: ".date("F d Y H:i:s", $COF_f_time ).
+    "\n  Compiled to JavaScript:     ".date ("F d Y H:i:s").
+    ($uglifyJS ? "\n  Minified using UglifyJS." : '').
+    ($_POST['includes'] ? "\n\n  Includes:\n  ".$_POST['includes']."\n" : '').
+    "\n*/\n\n".$store;
+  $toStore = utf8_encode(str_replace("\r","",$toStore));
+  file_put_contents($path,$toStore);
+  die();
 };
 
 // if we need to compile
 if($allowCacheCreation && ($JS_f_time < $COF_f_time || $JS_f_time < $CONFIG_f_time)){
-	// include once of the coffee-script compiler and the coffeemaker.js-script (client side)
-	// and always include a new script tag same as the original
-	$ticket = sha1(mt_rand().mt_rand());
-	file_put_contents('cache/__tickets.txt',$ticket."\n",FILE_APPEND);
-	$goAfterwards = '<script type="text/javascript" src="'.str_replace($_SERVER['DOCUMENT_ROOT'],'',__FILE__)
-	.'?rand='.rand().'&file='.$_GET['file'].'"></script>';
-	echo('window.cofeemaker ? window.coffeemaker.compile("'
-	.str_replace($_SERVER['DOCUMENT_ROOT'],'',__FILE__).'?passthru='.$_GET['file']
-	.'") && document.write(\''.$goAfterwards.'\') : (window.coffeemaker = {tocompile:"'
-	.str_replace($_SERVER['DOCUMENT_ROOT'],'',__FILE__)
-	.'?passthru='.$_GET['file'].'"'
-	.',ticket:"'.$ticket.'"'
-	.',allowIncludes:0'.$allowIncludes
-	.',splitVarDeclarations:0'.$splitVarDeclarations
-	.',keepCoffeeComments:0'.$keepCoffeeComments
-	.',keepBlankLines:0'.$keepBlankLines
-	.',splitConcatenations:0'.$splitConcatenations 
-	.',uglifyJS:0'.$uglifyJS 
-	.'}) && document.write'
-	.'(\'<script type="text/javascript" src="'.str_replace($_SERVER['DOCUMENT_ROOT'],'',dirname(__FILE__))
-	.'/coffee-script.js"></script><script type="text/javascript" src="'
-	.str_replace($_SERVER['DOCUMENT_ROOT'],'',dirname(__FILE__))
-	.'/coffeemaker.js"></script>'.$goAfterwards.'\');');
+  // include once of the coffee-script compiler and the coffeemaker.js-script (client side)
+  // and always include a new script tag same as the original
+  $ticket = sha1(mt_rand().mt_rand());
+  file_put_contents('cache/__tickets.txt',$ticket."\n",FILE_APPEND);
+  $goAfterwards = '<script type="text/javascript" src="'.str_replace($_SERVER['DOCUMENT_ROOT'],'',__FILE__)
+  .'?rand='.rand().'&file='.$_GET['file'].'"></script>';
+  echo('window.cofeemaker ? window.coffeemaker.compile("'
+  .str_replace($_SERVER['DOCUMENT_ROOT'],'',__FILE__).'?passthru='.$_GET['file']
+  .'") && document.write(\''.$goAfterwards.'\') : (window.coffeemaker = {tocompile:"'
+  .str_replace($_SERVER['DOCUMENT_ROOT'],'',__FILE__)
+  .'?passthru='.$_GET['file'].'"'
+  .',ticket:"'.$ticket.'"'
+  .',allowIncludes:0'.$allowIncludes
+  .',splitVarDeclarations:0'.$splitVarDeclarations
+  .',keepCoffeeComments:0'.$keepCoffeeComments
+  .',keepBlankLines:0'.$keepBlankLines
+  .',splitConcatenations:0'.$splitConcatenations 
+  .',uglifyJS:0'.$uglifyJS 
+  .'}) && document.write'
+  .'(\'<script type="text/javascript" src="'.str_replace($_SERVER['DOCUMENT_ROOT'],'',dirname(__FILE__))
+  .'/coffee-script.js"></script><script type="text/javascript" src="'
+  .str_replace($_SERVER['DOCUMENT_ROOT'],'',dirname(__FILE__))
+  .'/coffeemaker.js"></script>'.$goAfterwards.'\');');
 }
 // otherwise spit out the coffee
 else {
-	echo(file_get_contents(dirname(__FILE__).'/cache'.str_replace('.cof','.js',str_replace('..','',$_GET['file']))));
+  echo(file_get_contents(dirname(__FILE__).'/cache'.str_replace('.cof','.js',str_replace('..','',$_GET['file']))));
 };
